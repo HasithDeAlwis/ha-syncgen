cb2f409758dd9408d3200e476b68260ac60255d9
HasithDeAlwis <hasithde24@gmail.com>
2025-07-24 21:11:39 -0400

    feat(core): create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("ha-syncgen v0.1.0")
+	fmt.Println("A declarative tool for scaffolding high-availability PostgreSQL infrastructure")
+	os.Exit(0)
+}
