5cc7ddcd187ff5611bf3dcfd9708786fcfb0f5ba
HasithDeAlwis <hasithde24@gmail.com>
2025-07-26 14:33:30 -0400

    feat(internal/config): create YAML parser

---
+++ internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Primary struct {
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
+}
+
+type Replica struct {
+	Host         string `yaml:"host"`
+	SyncInterval string `yaml:"sync_interval"`
+}
+
+type Config struct {
+	Primary  Primary   `yaml:"primary"`
+	Replicas []Replica `yaml:"replicas"`
+}
+
+func Parse(filename string) (*Config, error) {
+	var config Config
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	if err := Validate(&config); err != nil {
+		return nil, fmt.Errorf("validation error: %w", err)
+	}
+
+	return &config, nil
+}
