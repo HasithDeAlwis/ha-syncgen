22da78a64d6e2840517e25c4efd75cc8a961c8fe
HasithDeAlwis <hasithde24@gmail.com>
2025-08-29 16:17:26 -0400

    style(generator): lint primary.go and datadog.go

--- internal/generator/datadog.go
+++ internal/generator/datadog.go
@@ -1,5 +1,3 @@
-
-
 package generator
 
 import (
@@ -7,45 +5,44 @@ import (
 	"text/template"
 )
 
-
 func (g *Generator) generateDatadogFiles(installTmpl, sqlTmpl, confTmpl *template.Template) error {
-   ddDir := filepath.Join(g.outputDir, "datadog")
-   specs := []FileSpec{
-	   {
-		   Tmpl:     installTmpl,
-		   Dir:      ddDir,
-		   Filename: "datadog-install.sh",
-		   Data: map[string]interface{}{
-			   "DataDogApiKey": g.config.Monitoring.Datadog.ApiKey,
-			   "DataDogSite":   g.config.Monitoring.Datadog.Site,
-		   },
-	   },
-	   {
-		   Tmpl:     sqlTmpl,
-		   Dir:      ddDir,
-		   Filename: "datadog.sql",
-		   Data: map[string]interface{}{
-			   "Password": g.config.Primary.DbPassword,
-		   },
-	   },
-	   {
-		   Tmpl:     confTmpl,
-		   Dir:      ddDir,
-		   Filename: "datadog-conf.yaml",
-		   Data: map[string]interface{}{
-			   "Host":          g.config.Primary.Host,
-			   "Port":          g.config.Primary.Port,
-			   "DbName":        g.config.Primary.DbName,
-			   "DbUser":        g.config.Primary.DbUser,
-			   "DbPassword":    g.config.Primary.DbPassword,
-			   "Datadirectory": g.config.Primary.DataDirectory,
-		   },
-	   },
-   }
-   for _, spec := range specs {
-	   if err := g.renderTemplate(spec.Tmpl, spec.Dir, spec.Filename, spec.Data); err != nil {
-		   return err
-	   }
-   }
-   return nil
+	ddDir := filepath.Join(g.outputDir, "datadog")
+	specs := []FileSpec{
+		{
+			Tmpl:     installTmpl,
+			Dir:      ddDir,
+			Filename: "datadog-install.sh",
+			Data: map[string]interface{}{
+				"DataDogApiKey": g.config.Monitoring.Datadog.ApiKey,
+				"DataDogSite":   g.config.Monitoring.Datadog.Site,
+			},
+		},
+		{
+			Tmpl:     sqlTmpl,
+			Dir:      ddDir,
+			Filename: "datadog.sql",
+			Data: map[string]interface{}{
+				"Password": g.config.Primary.DbPassword,
+			},
+		},
+		{
+			Tmpl:     confTmpl,
+			Dir:      ddDir,
+			Filename: "datadog-conf.yaml",
+			Data: map[string]interface{}{
+				"Host":          g.config.Primary.Host,
+				"Port":          g.config.Primary.Port,
+				"DbName":        g.config.Primary.DbName,
+				"DbUser":        g.config.Primary.DbUser,
+				"DbPassword":    g.config.Primary.DbPassword,
+				"Datadirectory": g.config.Primary.DataDirectory,
+			},
+		},
+	}
+	for _, spec := range specs {
+		if err := g.renderTemplate(spec.Tmpl, spec.Dir, spec.Filename, spec.Data); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- internal/generator/primary.go
+++ internal/generator/primary.go
@@ -1,4 +1,3 @@
-
 package generator
 
 import (
@@ -6,55 +5,54 @@ import (
 	"text/template"
 )
 
-
 // generatePrimaryFiles creates configuration files for the primary PostgreSQL server
 func (g *Generator) generatePrimaryFiles(pgHbaTmpl, postgresqlConfTmpl, setupPrimaryTmpl *template.Template) error {
-   primaryDir := filepath.Join(g.outputDir, "primary")
-   specs := []FileSpec{
-	   {
-		   Tmpl:     postgresqlConfTmpl,
-		   Dir:      primaryDir,
-		   Filename: "postgresql.conf.custom",
-		   Data: map[string]interface{}{
-			   "WalLevel":            g.config.Options.WalLevel,
-			   "HotStandby":          g.config.Options.HotStandby,
-			   "SynchronousCommit":   g.config.Options.SynchronousCommit,
-			   "MaxWalSenders":       g.config.Options.MaxWalSenders,
-			   "MaxReplicationSlots": len(g.config.Replicas) + 2,
-			   "WalKeepSize":         g.config.Options.WalKeepSize,
-			   "Port":                g.config.Primary.Port,
-			   "HasMonitoring":       g.config.Monitoring.Datadog.Enabled,
-		   },
-	   },
-	   {
-		   Tmpl:     pgHbaTmpl,
-		   Dir:      primaryDir,
-		   Filename: "pg_hba.conf.custom",
-		   Data: map[string]interface{}{
-			   "ReplicationUser": g.config.Primary.ReplicationUser,
-			   "Replicas":        g.config.Replicas,
-		   },
-	   },
-	   {
-		   Tmpl:     setupPrimaryTmpl,
-		   Dir:      primaryDir,
-		   Filename: "setup_primary.sh",
-		   Data: map[string]interface{}{
-			   "PrimaryHost":         g.config.Primary.Host,
-			   "PrimaryPort":         g.config.Primary.Port,
-			   "DbUser":              g.config.Primary.DbUser,
-			   "DbName":              g.config.Primary.DbName,
-			   "ReplicationUser":     g.config.Primary.ReplicationUser,
-			   "ReplicationPassword": g.config.Primary.ReplicationPassword,
-			   "Replicas":            g.config.Replicas,
-			   "DataDirectory":       g.config.Primary.DataDirectory,
-		   },
-	   },
-   }
-   for _, spec := range specs {
-	   if err := g.renderTemplate(spec.Tmpl, spec.Dir, spec.Filename, spec.Data); err != nil {
-		   return err
-	   }
-   }
-   return nil
+	primaryDir := filepath.Join(g.outputDir, "primary")
+	specs := []FileSpec{
+		{
+			Tmpl:     postgresqlConfTmpl,
+			Dir:      primaryDir,
+			Filename: "postgresql.conf.custom",
+			Data: map[string]interface{}{
+				"WalLevel":            g.config.Options.WalLevel,
+				"HotStandby":          g.config.Options.HotStandby,
+				"SynchronousCommit":   g.config.Options.SynchronousCommit,
+				"MaxWalSenders":       g.config.Options.MaxWalSenders,
+				"MaxReplicationSlots": len(g.config.Replicas) + 2,
+				"WalKeepSize":         g.config.Options.WalKeepSize,
+				"Port":                g.config.Primary.Port,
+				"HasMonitoring":       g.config.Monitoring.Datadog.Enabled,
+			},
+		},
+		{
+			Tmpl:     pgHbaTmpl,
+			Dir:      primaryDir,
+			Filename: "pg_hba.conf.custom",
+			Data: map[string]interface{}{
+				"ReplicationUser": g.config.Primary.ReplicationUser,
+				"Replicas":        g.config.Replicas,
+			},
+		},
+		{
+			Tmpl:     setupPrimaryTmpl,
+			Dir:      primaryDir,
+			Filename: "setup_primary.sh",
+			Data: map[string]interface{}{
+				"PrimaryHost":         g.config.Primary.Host,
+				"PrimaryPort":         g.config.Primary.Port,
+				"DbUser":              g.config.Primary.DbUser,
+				"DbName":              g.config.Primary.DbName,
+				"ReplicationUser":     g.config.Primary.ReplicationUser,
+				"ReplicationPassword": g.config.Primary.ReplicationPassword,
+				"Replicas":            g.config.Replicas,
+				"DataDirectory":       g.config.Primary.DataDirectory,
+			},
+		},
+	}
+	for _, spec := range specs {
+		if err := g.renderTemplate(spec.Tmpl, spec.Dir, spec.Filename, spec.Data); err != nil {
+			return err
+		}
+	}
+	return nil
 }
